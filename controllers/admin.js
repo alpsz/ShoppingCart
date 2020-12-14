ca15f228ccae714bf60e7773367d33c1b8ca3b38
alpsz <alpesh200720@gmail.com>
2020-12-14 11:57:56 +0530

    Complete the crud operations using file as a database

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,7 +7,11 @@ module.exports.getAddProduct = (req, res, next) => {
     
     //res.sendFile(path.join(rootDir,'views', "add-product.html"));
     //const products = Product.fetchAll();
-    res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+    res.render('admin/edit-product', { 
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+        });
 }
 
 module.exports.postAddProduct = (req,res, next) =>{
@@ -15,9 +19,40 @@ module.exports.postAddProduct = (req,res, next) =>{
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+}
+
+module.exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode){
+        res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if(!product){
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', { 
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product,
+        });
+    })   
+}
+
+module.exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
+
 }
 
 module.exports.getProducts =  (req, res, next) => {
@@ -28,4 +63,10 @@ module.exports.getProducts =  (req, res, next) => {
             path: '/admin/products'
           });
     });   
+}
+
+module.exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
 }
--- controllers/shop.js
+++ controllers/shop.js
@@ -16,7 +16,8 @@ module.exports.getProducts = (req, res, next) => {
 module.exports.getProduct = (req, res, next) =>{
     const prodId = req.params.productId;
     Product.findById(prodId, product => {
-        res.render('shop/product-detail',{product : product,
+        res.render('shop/product-detail',{
+            product : product,
             pageTitle : product.title,
             path: '/products'});
     })
@@ -31,15 +32,36 @@ module.exports.getIndex = (req, res,next) => {
             path: '/'
           });
     });   
-}
+};
 
 
 module.exports.getCart = (req,res, next) => {
-    res.render('shop/cart', {
-        path:'/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for(product of products) {
+                const cartProductData = cart.products.find(prod=> prod.id === product.id);
+                if(cartProductData){
+                    cartProducts.push({productData: product, qty: cartProductData.qty});
+                }   
+            }
+            res.render('shop/cart', {
+                path:'/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        });
+     
     });
-}
+    
+};
+
+
+module.exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+};
 
 
 module.exports.postCart = (req, res, next) => {
@@ -50,6 +72,15 @@ module.exports.postCart = (req, res, next) => {
     res.redirect('/cart');  
 };
 
+
+module.exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        res.redirect('/cart');
+    });
+    
+}
 
 module.exports.getOrders = (req,res, next) => {
     res.render('shop/orders', {
